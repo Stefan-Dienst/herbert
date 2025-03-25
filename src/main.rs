d2ae3e9327ac9f88a429eb28751dfe3209dca823
Stefan Dienst <stefan.dienst7@gmail.com>
2025-03-25 17:51:05 +0100

    parse first kafka request

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,42 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+use byteorder::{BigEndian, ReadBytesExt};
+use std::io::Cursor;
+
+#[derive(Debug)]
+struct RequestHeader {
+    request_api_key: i16,
+    request_api_version: i16,
+    correlation_id: i32,
+    client_id: String,
+}
+
+impl RequestHeader {
+    fn parse(buffer: &[u8; 512]) -> Self {
+        let mut cursor = Cursor::new(&buffer);
+
+        let size = cursor.read_i32::<BigEndian>().unwrap();
+
+        let request_api_key = cursor.read_i16::<BigEndian>().unwrap();
+        let request_api_version = cursor.read_i16::<BigEndian>().unwrap();
+        let correlation_id = cursor.read_i32::<BigEndian>().unwrap();
+        let size_of_client_id = cursor.read_i16::<BigEndian>().unwrap();
+
+        let mut unparsed_client_id = vec![0; size_of_client_id.try_into().unwrap()];
+        cursor.read_exact(&mut unparsed_client_id).unwrap();
+
+        let client_id = String::from_utf8(unparsed_client_id).expect("Invalid utf8 sequence");
+        info!("This is the client id: {:?}", client_id);
+        RequestHeader {
+            request_api_key: request_api_key,
+            request_api_version: request_api_version,
+            correlation_id: correlation_id,
+            client_id: client_id,
+        }
+    }
+}
+
 fn handle_connection(mut stream: TcpStream) {
     info!("I have received a connection!");
     let mut buffer = [0; 512];
@@ -15,7 +51,8 @@ fn handle_connection(mut stream: TcpStream) {
                 break;
             }
             Ok(n) => {
-                info!("Received: {:?}", &buffer[..n])
+                let header = RequestHeader::parse(&buffer);
+                info!("{:?}", header)
             }
             Err(..) => {
                 error!("Error");
