92b4310180a944db37c7bd1d4f31b1988777c43f
Stefan Dienst <stefan.dienst7@gmail.com>
2025-03-25 08:01:45 +0100

    write a simple tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,48 @@
-use log::{info, warn, error};
+use log::{error, info};
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
 
+fn handle_connection(mut stream: TcpStream) {
+    info!("I have received a connection!");
+    let mut buffer = [0; 512];
 
-fn main() {
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(0) => {
+                info!("Client disconnected.");
+                break;
+            }
+            Ok(n) => {
+                info!("Received: {:?}", &buffer[..n])
+            }
+            Err(..) => {
+                error!("Error");
+                break;
+            }
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
     env_logger::init();
-    info!("test")
+    add(1, 2);
+    let adress = "127.0.0.1:9001";
+    info!("Starting the TCP server. Listening on {:?}", adress);
+    let listener = TcpListener::bind(adress)?;
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_connection(stream);
+            }
+            Err(..) => {
+                error!("Oh oh!");
+            }
+        }
+    }
+    Ok(())
 }
 
 fn add(x: i32, y: i32) -> i32 {
@@ -18,5 +57,4 @@ mod tests {
     fn test_add() {
         assert_eq!(add(1, 2), 1 + 2)
     }
-
 }
