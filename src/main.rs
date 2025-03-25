51d65b02141f4fd23e3058155ef066935d584162
Stefan Dienst <stefan.dienst7@gmail.com>
2025-03-25 07:22:34 +0100

    setup first test

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,18 @@ fn main() {
     env_logger::init();
     info!("test")
 }
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 1 + 2)
+    }
+
 }
