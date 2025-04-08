1ec97153aa75b2393919ca070f922a60a2ea9aff
Stefan Dienst <stefan.dienst7@gmail.com>
2025-04-08 17:52:04 +0200

    support producing with the cli

--- src/bin/herber-cli.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("Hello from the second executable!");
-}
-
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use bytes::BufMut;
 use kafka_protocol::messages::api_versions_response::ApiVersion;
 use kafka_protocol::messages::metadata_request::MetadataRequestTopic;
 use kafka_protocol::messages::metadata_response::MetadataResponseBroker;
+use kafka_protocol::messages::metadata_response::MetadataResponsePartition;
 use kafka_protocol::messages::metadata_response::MetadataResponseTopic;
+use kafka_protocol::messages::request_header;
 use kafka_protocol::messages::ApiKey as ApiKey;
 use kafka_protocol::messages::ApiKey as RequestKind;
 use kafka_protocol::messages::ApiVersionsRequest;
 use kafka_protocol::messages::ApiVersionsResponse;
+use kafka_protocol::messages::BrokerId;
 use kafka_protocol::messages::FindCoordinatorRequest;
 use kafka_protocol::messages::FindCoordinatorResponse;
 use kafka_protocol::messages::ListGroupsRequest;
@@ -14,6 +17,7 @@ use kafka_protocol::messages::MetadataRequest;
 use kafka_protocol::messages::MetadataResponse;
 use kafka_protocol::messages::OffsetFetchRequest;
 use kafka_protocol::messages::OffsetFetchResponse;
+use kafka_protocol::messages::ProduceRequest;
 use kafka_protocol::messages::ResponseHeader;
 use kafka_protocol::messages::TopicName;
 use kafka_protocol::protocol::buf::ByteBuf;
@@ -89,6 +93,8 @@ fn handle_connection(mut stream: TcpStream) {
                 let api_key = new_buf.peek_bytes(0..2).get_i16();
                 let api_version = new_buf.peek_bytes(2..4).get_i16();
                 let header_version = ApiKey::try_from(api_key).unwrap().request_header_version(api_version);
+                info!("This is the header version: {:?}", header_version);
+
 
                 let header = RequestHeader::decode(&mut new_buf, header_version).unwrap();
                 let api_key = ApiKey::try_from(header.request_api_key);
@@ -98,7 +104,9 @@ fn handle_connection(mut stream: TcpStream) {
                 info!("This api version mannn: {:?}", api_version);
 
                 let mut response_buffer = BytesMut::new();
-                let response_header = ResponseHeader::default();
+                let mut response_header = ResponseHeader::default();
+                // Set the response correlation_id to the one of the request match them.
+                response_header.correlation_id = header.correlation_id;
                 let mut size = response_header.compute_size(header_version).unwrap();
 
                 match api_key {
@@ -168,7 +176,10 @@ fn handle_connection(mut stream: TcpStream) {
 
                         let mut topic = MetadataResponseTopic::default();
                         topic.name = Some(TopicName::from(StrBytes::from_string("foobar".to_string())));
-                        response.topics.push(topic);
+                        let mut partition = MetadataResponsePartition::default();
+                        partition.leader_id = (1).into();
+                        topic.partitions.push(partition);
+                        // response.topics.push(topic);
 
                         let mut broker = MetadataResponseBroker::default();
                         broker.node_id = (1).into();
@@ -182,7 +193,11 @@ fn handle_connection(mut stream: TcpStream) {
 
                         response_buffer.put_u32(size as u32); 
                         let _ = response_header.encode(&mut response_buffer, header_version);
-                        let _ = response.encode(&mut response_buffer, api_version);
+                        let _ = response.encode(&mut response_buffer, 1);
+                    },
+                    Ok(ApiKey::Produce) => {
+                        let a = ProduceRequest::decode(&mut Bytes::from(new_buf), header.request_api_version);
+                        dbg!(a);
                     }
                     _ => {info!("Something unexpected happend.");}
                 }
