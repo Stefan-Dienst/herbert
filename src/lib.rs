47819a1f2e7e39a7ff4467f5a8bcddbcb1c85b33
Stefan Dienst <stefan.dienst7@gmail.com>
2025-07-22 22:04:42 +0200

    add offset manager

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod client;
 pub mod kafka_api;
+pub mod offset_manager;
 pub mod server;
 pub mod storage;
 pub mod topic_manager;
---
+++ src/offset_manager.rs
@@ -0,0 +1,72 @@
+use anyhow::Result;
+use std::{collections::HashMap, sync::RwLock};
+
+struct OffsetManager {
+    offsets: RwLock<HashMap<(String, String), i64>>,
+}
+
+impl OffsetManager {
+    pub fn new() -> Self {
+        Self {
+            offsets: RwLock::new(HashMap::new()),
+        }
+    }
+
+    pub fn get_offset(&self, consumer_group: &str, topic: &str) -> Result<i64> {
+        self.offsets
+            .read()
+            .map_err(|e| anyhow::anyhow!("RwLock poisoned: {}", e))?
+            .get(&(consumer_group.to_string(), topic.to_string()))
+            .copied()
+            .ok_or_else(|| {
+                anyhow::anyhow!(
+                    "Offset not found for consumer group '{}' and topic '{}'",
+                    consumer_group,
+                    topic
+                )
+            })
+    }
+
+    pub fn set_offset(&self, consumer_group: &str, topic: &str, offset: i64) -> Result<()> {
+        self.offsets
+            .write()
+            .map_err(|e| anyhow::anyhow!("RwLock poisoned: {}", e))?
+            .insert((consumer_group.to_string(), topic.to_string()), offset);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let offset_manager = OffsetManager::new();
+        assert!(offset_manager.offsets.read().unwrap().is_empty())
+    }
+
+    #[test]
+    fn test_set_offset() {
+        let offset_manager = OffsetManager::new();
+        offset_manager.set_offset("test", "foobar", 10);
+        assert_eq!(
+            offset_manager
+                .offsets
+                .read()
+                .unwrap()
+                .get(&("test".to_string(), "foobar".to_string()))
+                .unwrap(),
+            &10
+        )
+    }
+
+    #[test]
+    fn test_get_offset() {
+        let offset_manager = OffsetManager::new();
+        let offset = 10;
+        offset_manager.set_offset("test", "foobar", offset);
+        let got_offset = offset_manager.get_offset("test", "foobar");
+        assert_eq!(offset, got_offset.unwrap())
+    }
+}
