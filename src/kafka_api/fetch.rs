62498e03328d05d8cf36b3fe1836cbda27e6e2cc
Stefan Dienst <stefan.dienst7@gmail.com>
2026-03-10 17:12:41 +0100

    add a comment on max bytes

--- src/kafka_api/fetch.rs
+++ src/kafka_api/fetch.rs
@@ -13,6 +13,8 @@ pub fn create_fetch_request(topic: &str, max_messages: i32, fetch_offset: i64) -
     let mut fetch_request = FetchRequest::default();
 
     // FIXME: Max bytes does not work. Herbet can't decode it somehow.
+    // If I use a different api version things break down. Kafka protocol is
+    // not properly handled...
     fetch_request.max_bytes = max_messages;
 
     let mut fetch_topic = FetchTopic::default();
